8b2958a726da3ee63bfc6f39aa3997c96c7f2b6e
Hourann Bosci <hourannb@dview.net>
2013-09-06 09:20:09 +0800

    initial commit, first attempt at wrangling a dataset
    PDF files, it should be said, are not anyone's friend.

---
+++ data.js
@@ -0,0 +1,147 @@
+data = {
+  "pollution": {
+    // revenue of 13519, minus compensation measures
+    "alp": [{
+      "label": "Carbon tax",
+      "revenue": 6048
+    }],
+    "lib": [{
+      "label": "Direct-action emissions reduction fund",
+      "spending": 1550
+    }]
+  },
+  "miningtax": {
+    "alp": [{
+      "label": "Mining tax",
+      "revenue": 3700
+    }]
+  },
+  "businesstax": {
+    "lib": [
+      {
+        "label": "Instant asset write-off",
+        "revenue": 2905
+      },
+      {
+        "label": "Interest withholding tax",
+        "revenue": 405
+      },
+      {
+        "label": "Tax loss carry back",
+        "revenue": 950
+      },
+      {
+        "label": "Accelerated depreciation for cars",
+        "revenue": 425
+      },
+      {
+        "label": "Paid parental leave levy",
+        "revenue": 4400
+      },
+      {
+        "label": "Company tax rate cut",
+        "spending": 4900
+      }
+    ]
+  },
+  "families": {
+    "alp": [
+      /* TODO: not really family? {
+        "label": "Supplementary welfare allowance",
+        "spending": 1106
+      },*/
+      /* and where to put this one? {
+        "label": "Cuts to car industry subsidies",
+        "revenue": 500
+      }*/
+      {
+        "label": "Schoolkids Bonus",
+        "spending": 4641
+      },
+      {
+        "label": "Paid parental leave",
+        "spending": 3665
+      }
+    ],
+    "lib": [
+      {
+        "label": "Paid parental leave",
+        "spending": 9802
+      }
+    ]
+  },
+  "superannuation": {
+    "alp": [
+      {
+        "label": "12% super guarantee",
+        "spending": 1640
+      },
+      {
+        "label": "Low Income Super Contribution",
+        "spending": 3722
+      }
+    ]
+  },
+  "infrastructure": {
+    "alp": [
+      {
+        "label": "Rail projects in Brisbane, Melbourne, Perth",
+        "spending": (453+75+100)
+      }
+    ],
+    "lib": [
+      {
+        "label": "Road projects in Sydney, Melbourne, Brisbane",
+        "spending": 4000
+      }
+    ]
+  },
+  "aid": {
+    "lib": [
+      {
+        "label": "Foreign aid cuts",
+        "revenue": 4500
+      }
+    ]
+  },
+  "regions": {
+    "alp": [
+      {
+        "label": "Murray-Darling Water Buybacks (additional spending)",
+        "revenue": 650
+      },
+      {
+        "label": "Regional Infrastructure Fund",
+        "spending": 2485
+      }
+    ]
+  },
+  "asylum": {
+    "lib": [
+      {
+        "label": "Savings from reducing illegal arrivals",
+        "revenue": 1088
+      },
+      {
+        "label": "Savings from lowered humanitarian migration",
+        "revenue": 1269
+      },
+    ]
+  },
+  "publicservice": {
+    "lib": [
+      {
+        "label": "Sacking public servants",
+        "revenue": 5212
+      },
+      {
+        "label": "Cuts to existing public servant parental leave",
+        "revenue": 1229
+      },
+      {
+        "label": "Reduced political advertising",
+        "revenue": 428
+      }
+    ]
+  }
+}
