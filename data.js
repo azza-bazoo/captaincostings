6a657783eecd12a9d900b6b8f924f89c06496bb0
Hourann Bosci <hourannb@dview.net>
2013-09-06 11:51:46 +0800

    add descriptions and re-sort somewhat

--- data.js
+++ data.js
@@ -1,43 +1,27 @@
 data = {
-  "pollution": {
-    // revenue of 13519, minus compensation measures
-    "alp": [{
-      "label": "Carbon tax",
-      "revenue": 6048
-    }],
-    "lib": [{
-      "label": "Direct-action emissions reduction fund",
-      "spending": 1550
-    }]
-  },
-  "miningtax": {
-    "alp": [{
-      "label": "Mining tax",
-      "revenue": 3700
-    }]
-  },
-  "businesstax": {
+  "bigbusiness": {
+    "title": "Big business",
+    "desc": "Labor&#8217;s carbon and mining taxes will pay for most of its other policies (and reduce pollution). Liberal policy will replace that revenue with spending on direct action and on a tax cut for all companies.",
+    "alp": [
+      {
+        // revenue of 13519, minus compensation measures
+        "label": "Carbon tax",
+        "revenue": 6048
+      },
+      {
+        "label": "Mining tax",
+        "revenue": 3700
+      }
+    ],
     "lib": [
-      {
-        "label": "Instant asset write-off",
-        "revenue": 2905
-      },
-      {
-        "label": "Interest withholding tax",
-        "revenue": 405
-      },
-      {
-        "label": "Tax loss carry back",
-        "revenue": 950
-      },
-      {
-        "label": "Accelerated depreciation for cars",
-        "revenue": 425
-      },
       {
         "label": "Paid parental leave levy",
         "revenue": 4400
       },
+      {
+        "label": "Direct-action emissions reduction fund",
+        "spending": 1550
+      }
       {
         "label": "Company tax rate cut",
         "spending": 4900
@@ -45,6 +29,8 @@ data = {
     ]
   },
   "families": {
+    "title": "Families",
+    "desc": "The Liberal paid parental leave scheme will cost more than twice as much as Labor&#8217;s scheme, funded by cutting the existing Schoolkids Bonus and introducing a levy on large employers.",
     "alp": [
       /* TODO: not really family? {
         "label": "Supplementary welfare allowance",
@@ -70,7 +56,31 @@ data = {
       }
     ]
   },
+  "smallbusiness": {
+    "title": "Small business",
+    "desc": "The Liberals propose to cancel various small business tax cuts introduced by Labor.",
+    "lib": [
+      {
+        "label": "Instant asset write-off",
+        "revenue": 2905
+      },
+      {
+        "label": "Interest withholding tax",
+        "revenue": 405
+      },
+      {
+        "label": "Tax loss carry back",
+        "revenue": 950
+      },
+      {
+        "label": "Accelerated depreciation for cars",
+        "revenue": 425
+      },
+    ]
+  },
   "superannuation": {
+    "title": "Superannuation",
+    "desc": "A key part of Labor&#8217;s mining tax package was an increase in everyone&#8217;s level of super, along with an additional boost for low-income earners.",
     "alp": [
       {
         "label": "12% super guarantee",
@@ -83,10 +93,12 @@ data = {
     ]
   },
   "infrastructure": {
+    "title": "Infrastructure",
+    "desc": "Tony Abbott wants to be the &#8220;infrastructure prime minister&#8221;, by cutting spending on rail and funding WestConnex, East-West Link, and the Brisbane Gateway instead.",
     "alp": [
       {
         "label": "Rail projects in Brisbane, Melbourne, Perth",
-        "spending": (453+75+100)
+        "spending": 628
       }
     ],
     "lib": [
@@ -97,51 +109,47 @@ data = {
     ]
   },
   "aid": {
+    "title": "Foreign Aid",
+    "desc": "To fund its road commitments, a Liberal government would significantly cut Australia&#8217;s xxxbn foreign aid budget in half.",
     "lib": [
       {
-        "label": "Foreign aid cuts",
+        "label": "Cuts to foreign aid",
         "revenue": 4500
       }
     ]
   },
-  "regions": {
-    "alp": [
+  "publicservice": {
+    "title": "The public service",
+    "desc": "Most of the other proposed Liberal savings come from reduced public sector spending.",
+    "lib": [
       {
-        "label": "Murray-Darling Water Buybacks (additional spending)",
-        "revenue": 650
+        "label": "Not replacing public servants as they retire",
+        "saving": 5212
       },
+      {
+        "label": "Cuts to existing public servant parental leave",
+        "saving": 1229
+      },
+      {
+        "label": "Reduced political advertising",
+        "saving": 428
+      }
+    ]
+  },
+  "regions": {
+    "title": "Rural and regional areas",
+    "desc": "Labor has allocated funding for regional areas through an infrastructure fund and a deal with the states in the Murray basin, which the Liberal Party intends to cancel.",
+    "alp": [
       {
         "label": "Regional Infrastructure Fund",
         "spending": 2485
       }
-    ]
-  },
-  "asylum": {
+    ],
     "lib": [
       {
-        "label": "Savings from reducing illegal arrivals",
-        "revenue": 1088
+        "label": "Cuts to Murray-Darling Water Buybacks",
+        "savings": 650
       },
-      {
-        "label": "Savings from lowered humanitarian migration",
-        "revenue": 1269
-      },
-    ]
-  },
-  "publicservice": {
-    "lib": [
-      {
-        "label": "Sacking public servants",
-        "revenue": 5212
-      },
-      {
-        "label": "Cuts to existing public servant parental leave",
-        "revenue": 1229
-      },
-      {
-        "label": "Reduced political advertising",
-        "revenue": 428
-      }
     ]
   }
 }
